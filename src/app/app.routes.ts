4d8a3e422f44a86c108b2c4d539b61fd486aafa2
Garcia <200005@studierende.htl-donaustadt.at>
2024-11-25 09:13:31 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import {PageDemoComponent} from './page-demo/page-demo.component';
-import {PageHomeComponent} from './page-home/page-home.component';
 
-export const routes: Routes = [
-  { path: '', // default route
-    component: PageHomeComponent },
-  { path: 'home',
-    component: PageHomeComponent },
-  { path: 'page_demo',
-    component: PageDemoComponent },
-];
-
+export const routes: Routes = [];
