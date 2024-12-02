b5d043dfda78506cf4fd0c4b49b33246e3c137f1
210013 <210013@studierende.htl-donaustadt.at>
2024-12-02 09:56:11 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import {PageHomeComponent} from './page-home/page-home.component';
 import {PageAboutMePONTComponent} from './page-about-me-pont/page-about-me-pont.component';
 
 export const routes: Routes = [
-  { path: '', // default route
+  { path: '', // default rout
     component: PageHomeComponent },
   { path: 'home',
     component: PageHomeComponent },
