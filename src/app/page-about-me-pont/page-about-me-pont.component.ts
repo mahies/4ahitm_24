4299761e87ed8246d5e03f2529c160c7d024accb
ponta <190280@studierende.htl-donaustadt.at>
2024-12-02 09:18:46 +0100

    AboutMe Pontasch

--- src/app/page-about-me-pont/page-about-me-pont.component.ts
+++ src/app/page-about-me-pont/page-about-me-pont.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-pont',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-about-me-pont.component.html',
   styleUrl: './page-about-me-pont.component.scss'
 })
