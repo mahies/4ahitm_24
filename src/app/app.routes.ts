535d45208e66c15aaffe7a674beaf7b036b4b337
Kovacs <210015@Studierende.htl-donaustadt.at>
2024-12-02 09:58:27 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {PageDemoComponent} from './page-demo/page-demo.component';
 import {PageHomeComponent} from './page-home/page-home.component';
 import {PageAboutMePONTComponent} from './page-about-me-pont/page-about-me-pont.component';
+import {PageAboutMeKOVACSComponent} from "./page-about-me-kovacs/page-about-me-kovacs.component";
 
 export const routes: Routes = [
   { path: '', // default rout
@@ -12,5 +13,7 @@ export const routes: Routes = [
     component: PageDemoComponent },
   { path: 'page_pont',
     component: PageAboutMePONTComponent },
+  { path: 'page_kovacs',
+    component: PageAboutMeKOVACSComponent },
 ];
 
