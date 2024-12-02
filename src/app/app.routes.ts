c050770ff1b53d3ff052c1dae9fe24614b17150c
ponta <190280@studierende.htl-donaustadt.at>
2024-12-02 09:51:41 +0100

    AboutMe Pontasch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {PageDemoComponent} from './page-demo/page-demo.component';
 import {PageHomeComponent} from './page-home/page-home.component';
+import {PageAboutMePONTComponent} from './page-about-me-pont/page-about-me-pont.component';
 
 export const routes: Routes = [
   { path: '', // default route
@@ -9,5 +10,7 @@ export const routes: Routes = [
     component: PageHomeComponent },
   { path: 'page_demo',
     component: PageDemoComponent },
+  { path: 'page_pont',
+    component: PageAboutMePONTComponent },
 ];
 
