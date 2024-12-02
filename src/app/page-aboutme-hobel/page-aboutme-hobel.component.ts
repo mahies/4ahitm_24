456c7a87ea93245ed73cef82dda6ed00cd5e5c98
richard hobel <200012@studierende.htl-donaustadt.at>
2024-12-02 09:42:41 +0100

    intial commit

---
+++ src/app/page-aboutme-hobel/page-aboutme-hobel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-aboutme-hobel',
+  imports: [],
+  templateUrl: './page-aboutme-hobel.component.html',
+  styleUrl: './page-aboutme-hobel.component.scss'
+})
+export class PageAboutmeHobelComponent {
+
+}
