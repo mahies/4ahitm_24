e78b51f61522a77518ba244c7037ced2c88e6540
sevvicc <210042@studierende.htl-donaustadt.at>
2024-12-02 10:26:55 +0100

    About Me - Kovacic

---
+++ src/app/page-about-me-kovacic/page-about-me-kovacic.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-page-about-me-kovacic',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './page-about-me-kovacic.component.html',
+  standalone: true,
+  styleUrl: './page-about-me-kovacic.component.scss'
+})
+export class PageAboutMeKOVACICComponent {
+
+}
