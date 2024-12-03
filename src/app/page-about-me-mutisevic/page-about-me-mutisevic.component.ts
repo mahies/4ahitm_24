3115f14da843de571b532afaeb2050e8235d272e
adinocarti <200224@studierende.htl-donaustadt.at>
2024-12-03 14:35:25 +0100

    about me adin

--- src/app/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
+++ src/app/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-mutisevic',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './page-about-me-mutisevic.component.html',
+  standalone: true,
   styleUrl: './page-about-me-mutisevic.component.scss'
 })
 export class PageAboutMeMutisevicComponent {
