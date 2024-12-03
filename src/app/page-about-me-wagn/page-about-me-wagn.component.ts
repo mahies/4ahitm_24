29b4715909f46aac294297e8a3049fcd8853ad46
Carina <carina.wag19@gmail.com>
2024-12-03 14:27:46 +0100

    P01-32

--- src/app/page-about-me-wagn/page-about-me-wagn.component.ts
+++ src/app/page-about-me-wagn/page-about-me-wagn.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import {Carina, Person} from '../model/person';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-wagn',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-about-me-wagn.component.html',
   styleUrl: './page-about-me-wagn.component.scss'
 })
