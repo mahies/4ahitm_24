d425705d73caf397798901f44655922013d1a825
mahi <hies@htl-donaustadt.at>
2024-12-03 13:17:31 +0100

    add model, add const MINNIE, use const

--- src/app/page-demo/page-demo.component.ts
+++ src/app/page-demo/page-demo.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {Person, MINNIE} from '../model/person';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-demo',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './page-demo.component.html',
   styleUrl: './page-demo.component.scss',
   standalone: true
 })
 export class PageDemoComponent {
-
+  me: Person = MINNIE; //Variable me vom Typ Person = KONSTANTE MOUSE
 }
