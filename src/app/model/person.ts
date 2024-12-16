09c405e736fb3d20a6e6074b7336ad9c2b3ca5c9
Kovacs <210015@Studierende.htl-donaustadt.at>
2024-12-16 11:00:52 +0100

    Geburtstag & Geburtsdatum anzeigen

--- src/app/model/person.ts
+++ src/app/model/person.ts
@@ -55,7 +55,7 @@ export  const KOVACS : Person = {
   surname: "Kovacs",
   firstname: "Philip",
   urlImage: "assets/Kovacs_Bilder/Portrait_Kovacs.jpg",
-  birthday: new Date("2007-05-04"),
+  birthday: new Date("2007-05-04T14:35"),
   department: "ITEL Netzwerktechnik",
   email: "210015@studierende.htl-donaustadt.at",
   github_nickname: "PhilipKovacs",
