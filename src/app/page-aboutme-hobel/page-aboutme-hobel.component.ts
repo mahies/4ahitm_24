5aa0cdcde54a382e32f6ceaf7531833c5f4c1829
richard hobel <200012@studierende.htl-donaustadt.at>
2024-12-16 11:07:00 +0100

    med-11, med-21, med-30, med-31

--- src/app/page-aboutme-hobel/page-aboutme-hobel.component.ts
+++ src/app/page-aboutme-hobel/page-aboutme-hobel.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Person, MINNIE, HOBEL} from '../model/person';
+import {Person, MINNIE, HOBEL, MAJZEL} from '../model/person';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
