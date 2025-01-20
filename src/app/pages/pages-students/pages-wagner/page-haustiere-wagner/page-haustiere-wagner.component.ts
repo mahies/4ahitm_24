976b514314a4cbb1f725504de885eb24eac47343
Carina <carina.wag19@gmail.com>
2025-01-20 09:41:18 +0100

    P01-51

--- src/app/pages/pages-students/pages-wagner/page-haustiere-wagner/page-haustiere-wagner.component.ts
+++ src/app/pages/pages-students/pages-wagner/page-haustiere-wagner/page-haustiere-wagner.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-haustiere-wagner',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-haustiere-wagner.component.html',
   styleUrl: './page-haustiere-wagner.component.scss'
 })
