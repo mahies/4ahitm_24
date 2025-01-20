7468a9603e3d3ae4cae80cec60c869e9fbc1f223
Moritz-Schule <210032@studierende.htl-donaustadt.at>
2025-01-20 11:42:52 +0100

    Scrum 37 & 34 Teilweise

--- src/app/pages/pages-students/pages-metz/album.ts
+++ src/app/pages/pages-students/pages-metz/album.ts
@@ -12,11 +12,32 @@ export interface Album {
 export const TEST : Album = {
   titel: "Musteralbum",
   interpret: "Mustermusiker",
-  coverLink: "./asdasdasd",
+  coverLink: "assets/metzinger-bilder/Portrait_Metzinger.jpg",
   erscheinungsdatum: new Date("1928-11-18"),
   rymLink: "string",
   reviewH: "string",
   reviewP: "string",
 }
 
-export const ALBEN:Album[] = [TEST];
+export const TwinFantasy : Album = {
+  titel: "Twin Fantasy",
+  interpret: "CarSeatHeadrest",
+  coverLink: "assets/metzinger-bilder/Covers/TwinFantasy.jpg",
+  erscheinungsdatum: new Date("2018-02-16"),
+  rymLink: "string",
+  reviewH: "string",
+  reviewP: "string",
+}
+
+export const TexasJerusalem : Album = {
+  titel: "Twin Fantasy",
+  interpret: "Mustermusiker",
+  coverLink: "assets/metzinger-bilder/Covers/TexasJerusalem.jpg",
+  erscheinungsdatum: new Date("2001-05-15"),
+  rymLink: "string",
+  reviewH: "string",
+  reviewP: "string",
+}
+
+
+export const ALBEN:Album[] = [TwinFantasy, TexasJerusalem];
