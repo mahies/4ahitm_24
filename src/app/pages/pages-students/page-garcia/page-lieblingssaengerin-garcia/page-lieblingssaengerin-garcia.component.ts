d9fe367bf4cef571bf2887a102753e08926b21ed
rizi06 <200005@studierende.htl-donaustadt.at>
2025-01-21 14:14:07 +0100

    WEB-24

--- src/app/pages/pages-students/page-garcia/page-lieblingssaengerin-garcia/page-lieblingssaengerin-garcia.component.ts
+++ src/app/pages/pages-students/page-garcia/page-lieblingssaengerin-garcia/page-lieblingssaengerin-garcia.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {GARCIA, Person} from '../../../../model/person';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-lieblingssaengerin-garcia',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-lieblingssaengerin-garcia.component.html',
+  standalone: true,
   styleUrl: './page-lieblingssaengerin-garcia.component.scss'
 })
 export class PageLieblingssaengerinGARCIAComponent {
-
+  protected readonly me = GARCIA;
 }
+
+
