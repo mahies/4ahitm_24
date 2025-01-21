9bd5873ab7b29a2820556efd79a96074ae93aff2
210053 <210053@studierende.htl-donaustadt.at>
2025-01-21 13:16:43 +0100

    Back button update

--- src/app/pages/pages-students/page-novo/page-aboutme-novo/page-aboutme-novo.component.ts
+++ src/app/pages/pages-students/page-novo/page-aboutme-novo/page-aboutme-novo.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import {NOVO} from '../../../../model/person';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-page-aboutme-novo',
-  imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './page-aboutme-novo.component.html',
   standalone: true,
   styleUrl: './page-aboutme-novo.component.scss'
