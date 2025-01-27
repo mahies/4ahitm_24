3619174676d39b749ce279ea986aa0b654835515
adinocarti <200224@studierende.htl-donaustadt.at>
2025-01-27 08:47:56 +0100

    subpages fehler behoben

--- src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
@@ -13,22 +13,4 @@ export class PageMusicMutisevicComponent {
   Person= MUTISEVIC;
   protected readonly me = MUTISEVIC;
 
-  playboiCarti = {
-    name: 'Playboi Carti',
-    realName: 'Jordan Terrell Carter',
-    birthday: '13. September 1996',
-    birthplace: 'Atlanta, Georgia, USA',
-    genre: 'Hip-Hop, Rap, Trap',
-    debutAlbum: 'Die Lit (2018)',
-    latestAlbum: 'Whole Lotta Red (2020)',
-    imageUrl: 'https://link-zu-einem-playboi-carti-bild.com/bild.jpg',
-    description: `
-      Playboi Carti ist ein US-amerikanischer Rapper und Songwriter, der für seinen einzigartigen Sound, seine experimentellen Beats
-      und seinen minimalistischen Stil bekannt ist. Mit Hits wie "Magnolia" und "Shoota" hat er die Hip-Hop-Szene revolutioniert
-      und eine große Fangemeinde aufgebaut. Seine Alben, darunter "Whole Lotta Red", haben Kultstatus erreicht. 2024 wäre auch sein
-      neues Album "I AM MUSIC" erschienen, aber mal wieder lügte er seine Fans an ;(
-    `,
-    spotifyLink: 'https://open.spotify.com/artist/5f7VJjfbwm532GiveGC0ZK',
-    instagramLink: 'https://www.instagram.com/playboicarti/',
-  };
 }
