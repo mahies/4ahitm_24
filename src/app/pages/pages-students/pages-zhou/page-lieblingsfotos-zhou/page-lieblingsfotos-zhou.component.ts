452deaae817adddf7b1959eb7aa0b0fa863aed43
210013 <210013@studierende.htl-donaustadt.at>
2025-01-27 10:01:26 +0100

    subpage bugfix

--- src/app/pages/pages-students/pages-zhou/page-lieblingsfotos-zhou/page-lieblingsfotos-zhou.component.ts
+++ src/app/pages/pages-students/pages-zhou/page-lieblingsfotos-zhou/page-lieblingsfotos-zhou.component.ts
@@ -3,7 +3,6 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-lieblingsfotos-zhou',
-  imports: [RouterLink, RouterLinkActive],
   templateUrl: './page-lieblingsfotos-zhou.component.html',
   standalone: true,
   styleUrl: './page-lieblingsfotos-zhou.component.scss'
--- src/app/pages/pages-students/pages-zhou/page-lieblingsmusik-zhou/page-lieblingsmusik-zhou.component.ts
+++ src/app/pages/pages-students/pages-zhou/page-lieblingsmusik-zhou/page-lieblingsmusik-zhou.component.ts
@@ -3,7 +3,6 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-lieblingsmusik-zhou',
-  imports: [RouterLink, RouterLinkActive],
   templateUrl: './page-lieblingsmusik-zhou.component.html',
   standalone: true,
   styleUrl: './page-lieblingsmusik-zhou.component.scss'
