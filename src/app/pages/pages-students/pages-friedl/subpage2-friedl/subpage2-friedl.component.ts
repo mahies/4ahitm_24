8d1f67b5e3c7ae227d39adccd652be14a913773e
200006 <st.friedl04@gmail.com>
2025-01-27 11:46:30 +0100

    Buttons zum zurückgehen erstellen AHITM-34 AHITM-35

--- src/app/pages/pages-students/pages-friedl/subpage2-friedl/subpage2-friedl.component.ts
+++ src/app/pages/pages-students/pages-friedl/subpage2-friedl/subpage2-friedl.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import {Person, FRIEDL} from '../../../../model/person';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 
 @Component({
   selector: 'subpage2-friedl',
   templateUrl: './subpage2-friedl.component.html',
   styleUrl: './subpage2-friedl.component.scss',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   standalone: true
 })
 export class Subpage2FriedlComponent {
