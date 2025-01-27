b40809971372e60e103208222e941345e50b59ba
210013 <210013@studierende.htl-donaustadt.at>
2025-01-27 10:01:58 +0100

    subpage bugfix

--- src/app/pages/pages-students/pages-zhou/page-lieblingsfotos-zhou/page-lieblingsfotos-zhou.component.ts
+++ src/app/pages/pages-students/pages-zhou/page-lieblingsfotos-zhou/page-lieblingsfotos-zhou.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-
 @Component({
   selector: 'app-page-lieblingsfotos-zhou',
   templateUrl: './page-lieblingsfotos-zhou.component.html',
