c6ffac1f02d6f0a8d66e8305c71cf19f866977e6
inesschoen <210179@studierende.htl-donaustadt.at>
2025-02-21 22:51:27 +0100

    Subpage1 Hobbyys

--- src/app/pages/pages-students/page-about-me-schoengrundner/page-subpage1/subpage1-schoen.component.ts
+++ src/app/pages/pages-students/page-about-me-schoengrundner/page-subpage1/subpage1-schoen.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
-import {Person, SCHOENGRUNDNER} from '../../../../model/person';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 @Component({
   selector: 'subpage1-schoen',
   templateUrl: './subpage1-schoen.component.html',
-  styleUrl: './subpage1-schoen.component.scss',
+  standalone: true,
   imports: [
     RouterLink,
     RouterLinkActive
   ],
-  standalone: true
+  styleUrl: './subpage1-schoen.component.scss'
 })
 export class Subpage1SchoenComponent {
 
-  me_schoen: Person = SCHOENGRUNDNER;
-  protected readonly me = SCHOENGRUNDNER;
 }
-
