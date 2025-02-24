35e9e840a207249988b1d8cf874e0c00b37bb206
ponta <190280@studierende.htl-donaustadt.at>
2025-02-24 01:07:21 +0100

    SCRUM-29

--- src/app/pages/pages-students/pages-pont/page-hobbies-pont/page-hobbies-pont.component.ts
+++ src/app/pages/pages-students/pages-pont/page-hobbies-pont/page-hobbies-pont.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-hobbies-pont',
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './page-hobbies-pont.component.html',
   styleUrl: './page-hobbies-pont.component.scss'
 })
