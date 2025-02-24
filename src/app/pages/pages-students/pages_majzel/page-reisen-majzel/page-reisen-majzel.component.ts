2e4246151b26cd5a0d1fd35cab16925e0d59455f
david <davidmeisel54@gmail.com>
2025-02-24 10:44:48 +0100

    PS 31

--- src/app/pages/pages-students/pages_majzel/page-reisen-majzel/page-reisen-majzel.component.ts
+++ src/app/pages/pages-students/pages_majzel/page-reisen-majzel/page-reisen-majzel.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import {MAJZEL} from "../../../../model/person";
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-reisen-majzel',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-reisen-majzel.component.html',
   styleUrl: './page-reisen-majzel.component.scss'
 })
