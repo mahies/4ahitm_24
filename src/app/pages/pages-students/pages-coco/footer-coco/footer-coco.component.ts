8bba433d3b55bc34cf8497113fffbe313cb0c3fc
Corneliu <secrieri.corneliu@gmail.com>
2025-02-25 14:31:37 +0100

    Footer-SP3.2

---
+++ src/app/pages/pages-students/pages-coco/footer-coco/footer-coco.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-footer-coco',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './footer-coco.component.html',
+  styleUrl: './footer-coco.component.scss'
+})
+export class FooterCocoComponent {
+
+}
