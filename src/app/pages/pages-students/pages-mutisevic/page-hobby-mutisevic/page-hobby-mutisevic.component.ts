dde2d163a24978d11ca97bd0e9c4de521ed20cf7
adinocarti <200224@studierende.htl-donaustadt.at>
2025-02-25 14:27:22 +0100

    WEB-34 & 35 Header auf Hobby & Music

--- src/app/pages/pages-students/pages-mutisevic/page-hobby-mutisevic/page-hobby-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-hobby-mutisevic/page-hobby-mutisevic.component.ts
@@ -3,15 +3,17 @@ import {cr7} from '../cr7';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MUTISEVIC} from '../../../../model/person';
 import {carti} from '../carti';
+import {PageHeaderMutisevicComponent} from '../page-header-mutisevic/page-header-mutisevic.component';
 
 @Component({
     selector: 'app-page-hobby-mutisevic',
     templateUrl: './page-hobby-mutisevic.component.html',
     standalone: true,
-    imports: [
-        RouterLink,
-        RouterLinkActive
-    ],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    PageHeaderMutisevicComponent
+  ],
     styleUrl: './page-hobby-mutisevic.component.scss'
 })
 export class PageHobbyMutisevicComponent {
--- src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {carti} from '../carti';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MUTISEVIC} from '../../../../model/person';
+import {PageHeaderMutisevicComponent} from '../page-header-mutisevic/page-header-mutisevic.component';
 
 @Component({
   selector: 'app-page-music-mutisevic',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent],
   templateUrl: './page-music-mutisevic.component.html',
   standalone: true,
   styleUrl: './page-music-mutisevic.component.scss'
