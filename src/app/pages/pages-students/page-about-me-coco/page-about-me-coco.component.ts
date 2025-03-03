2a3d57a1b8f5b55b3f8aedd27f269f7a4ea8c7e5
Corneliu <secrieri.corneliu@gmail.com>
2025-03-03 11:15:13 +0100

    Footer-SP3.2

--- src/app/pages/pages-students/page-about-me-coco/page-about-me-coco.component.ts
+++ src/app/pages/pages-students/page-about-me-coco/page-about-me-coco.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {COCO} from '../../../model/person';
 import {HeaderCocoComponent} from '../pages-coco/header-coco/header-coco.component';
+import {FooterCocoComponent} from '../pages-coco/footer-coco/footer-coco.component';
 
 @Component({
   selector: 'app-page-about-me-coco',
   imports: [
     RouterLink,
     RouterLinkActive,
-    HeaderCocoComponent
+    HeaderCocoComponent,
+    FooterCocoComponent
   ],
   templateUrl: './page-about-me-coco.component.html',
   styleUrl: './page-about-me-coco.component.scss'
