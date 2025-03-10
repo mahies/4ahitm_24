1e05469a46d984b51612e5d39c18c4c2cb46e4f1
adinocarti <200224@studierende.htl-donaustadt.at>
2025-03-10 11:28:35 +0100

    WEB- 42 Icons für About me erstellt

--- src/app/pages/pages-students/pages-mutisevic/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
@@ -4,10 +4,11 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MUTISEVIC} from '../../../../model/person';
 import {PageHeaderMutisevicComponent} from '../page-header-mutisevic/page-header-mutisevic.component';
 import {PageFooterMutisevicComponent} from '../page-footer-mutisevic/page-footer-mutisevic.component';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-page-about-me-mutisevic',
-  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent, PageFooterMutisevicComponent],
+  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent, PageFooterMutisevicComponent, DatePipe],
   templateUrl: './page-about-me-mutisevic.component.html',
   standalone: true,
   styleUrl: './page-about-me-mutisevic.component.scss'
