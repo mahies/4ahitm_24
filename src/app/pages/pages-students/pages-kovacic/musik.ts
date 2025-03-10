04bd0d0aa93d4fc6759321fb76036eaf1954a9b7
vicik <210042@studierende.htl-donaustadt.at>
2025-03-10 22:29:07 +0100

    35 -KOVACIC

---
+++ src/app/pages/pages-students/pages-kovacic/musik.ts
@@ -0,0 +1,47 @@
+export interface Song {
+  title: string;
+  artist: string;
+  imgSrc: string;
+  spotifyLink: string;
+  youtubeLink: string;
+}
+
+
+export const songs = [
+  {
+    title: 'I Miss The Misery',
+    artist: 'Halestorm',
+    imgSrc: '/assets/kovacic_bilder/imissthemisery.jpg',
+    spotifyLink: 'https://open.spotify.com/intl-de/track/3gmEzilP9BzF45wIMvA16l',
+    youtubeLink: 'https://www.youtube.com/watch?v=YpJAmlnBxoA'
+  },
+  {
+    title: 'Over Each Other',
+    artist: 'Linkin Park',
+    imgSrc: '/assets/kovacic_bilder/overeachother.jpg',
+    spotifyLink: 'https://open.spotify.com/intl-de/album/09mU09ThtQmKfCgtEeiQZP',
+    youtubeLink: 'https://www.youtube.com/watch?v=fSHoePrnmMw'
+  },
+  {
+    title: 'Famous Last Words',
+    artist: 'My Chemical Romance',
+    imgSrc: '/assets/kovacic_bilder/famouslastwords.jpg',
+    spotifyLink: 'https://open.spotify.com/intl-de/track/2d6m2F4I7wCuAKtSsdhh83',
+    youtubeLink: 'https://www.youtube.com/watch?v=8bbTtPL1jRs'
+  },
+  {
+    title: 'Voices',
+    artist: 'Against The Current',
+    imgSrc: '/assets/kovacic_bilder/voices.jpg',
+    spotifyLink: 'https://open.spotify.com/intl-de/track/6iA4iQegpoPH7dOrPdxYur',
+    youtubeLink: 'https://www.youtube.com/watch?v=oXFEMV919Pk'
+  },
+  {
+    title: 'Darkside',
+    artist: 'Bring Me The Horizon',
+    imgSrc: '/assets/kovacic_bilder/darkside.jpg',
+    spotifyLink: 'https://open.spotify.com/intl-de/album/3E21iYJYv6lSkBkHBv3SKq',
+    youtubeLink: 'https://www.youtube.com/watch?v=3Nt37RGbVjo'
+  }
+];
+
