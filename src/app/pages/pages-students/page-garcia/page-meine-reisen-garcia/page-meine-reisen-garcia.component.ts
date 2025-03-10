d2759cd841211ade148ea3534ddd7ba7ea7e2384
rizi06 <200005@studierende.htl-donaustadt.at>
2025-03-10 09:52:10 +0100

    Scrum 31 Footer für alle Seiten

--- src/app/pages/pages-students/page-garcia/page-meine-reisen-garcia/page-meine-reisen-garcia.component.ts
+++ src/app/pages/pages-students/page-garcia/page-meine-reisen-garcia/page-meine-reisen-garcia.component.ts
@@ -3,14 +3,16 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 import {ReiseGar} from '../ReiseGar';
 import {SZA} from '../sza_info';
 import {PageHeaderGarciaComponent} from "../page-header-garcia/page-header-garcia.component";
+import {PageFooterGarciaComponent} from '../page-footer-garcia/page-footer-garcia.component';
 
 @Component({
   selector: 'app-page-meine-reisen-garcia',
-    imports: [
-        RouterLink,
-        RouterLinkActive,
-        PageHeaderGarciaComponent
-    ],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    PageHeaderGarciaComponent,
+    PageFooterGarciaComponent
+  ],
   templateUrl: './page-meine-reisen-garcia.component.html',
   standalone: true,
   styleUrl: './page-meine-reisen-garcia.component.scss'
--- src/app/pages/pages-students/page-garcia/page-vacations-garcia/page-vacations-garcia.component.ts
+++ src/app/pages/pages-students/page-garcia/page-vacations-garcia/page-vacations-garcia.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {GARCIA} from '../../../../model/person';
 import {PageHeaderGarciaComponent} from "../page-header-garcia/page-header-garcia.component";
+import {PageFooterGarciaComponent} from '../page-footer-garcia/page-footer-garcia.component';
 
 @Component({
   selector: 'app-page-vacations-garcia',
-    imports: [RouterLink,
-        RouterLinkActive, PageHeaderGarciaComponent],
+  imports: [RouterLink,
+    RouterLinkActive, PageHeaderGarciaComponent, PageFooterGarciaComponent],
   templateUrl: './page-vacations-garcia.component.html',
   standalone: true,
   styleUrl: './page-vacations-garcia.component.scss'
