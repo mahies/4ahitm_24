649078997afb2cbd7f977e94aceb78f7983463bd
KovacsPhilip <210015@studierende.htl-donaustadt.at>
2025-03-10 22:37:48 +0100

    SCRUM 77,78,79,80,81

--- src/app/pages/pages-students/pages-kovacs/flaggen.ts
+++ src/app/pages/pages-students/pages-kovacs/flaggen.ts
@@ -28,7 +28,7 @@ export const Kiribati : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Kiribati.jpg",
   population: "133.515",
   datum: new Date("1979-08-12"),
-  review: "",
+  review: "Meine absolute Lieblingsflagge. Wir haben wieder eine wunderbare Szenerie die vom abgebildeten Vogel getoppt wird. Komposition ist einfach idyllisch und repräsentiert die Inselnation ideal.",
   verhaeltnis: "1:2"
 
 }
@@ -38,7 +38,7 @@ export const Antigua_und_Barbuda : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Antigua_and_Barbuda.jpg",
   population: "94.298",
   datum: new Date("1967-03-27"),
-  review: "",
+  review: "Ich liebe ja prinzipiell Flaggen, die Landschaften darstellen, wie auch hier die Strand Szenerie bei Sonnenuntergang gezeigt wird. Farben funktionieren super. Einzige Frage: Warum ist der Himmer schwarz?",
   verhaeltnis: "2:3"
 
 }
@@ -48,7 +48,8 @@ export const BritischesTerr : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Britisches_Indi.jpg",
   population: "ca. 3500",
   datum: new Date("1990-12-8"),
-  review: "",
+  review: "\n" +
+    "Ist eine sehr besondere Flagge aufgrund der durchgehenden Wellenlinien. der Union Jack macht sie leider um einiges schlechter. Wappen sitzt aber top.",
   verhaeltnis: "1:2"
 
 }
@@ -59,7 +60,7 @@ export const AmerikanischSamoa : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/American_Samoa.jpg",
   population: "43.914",
   datum: new Date("1960-05-24"),
-  review: "",
+  review: "Epische Flagge aufgrund des auffallenden Adlers. Ist leider etwas zu detailliert für eine Flagge meiner Meinung nach, hat aber einen Platz in meiner Topliste verdient.",
   verhaeltnis: "1:2"
 
 }
@@ -70,7 +71,7 @@ export const Bhutan : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Bhutan.jpg",
   population: "787.424 ",
   datum: new Date("1949-00-00"),
-  review: "",
+  review: "Bhutan ist natürlich was super besonderes, eine der wenigen Flaggen mit orange, eine der wenigen, die man sich gleich merkt & auffällt. Besonders ist, dass nicht definiert ist, wie genau der Drache auf der Flagge auszusehen hat. Es gibt also 100te Versionen.",
   verhaeltnis: "2:3"
 
 }
@@ -81,7 +82,7 @@ export const Estonia : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Estonia.jpg",
   population: "1.366.000 ",
   datum: new Date("1884-07-4"),
-  review: "",
+  review: "Manchmal ist weniger einfach mehr. Estland hat hier abgeräumt. Die Farben sind markant & funktionieren super miteinander. Ich liebe abgesehen davon auch das Auftreten von Schwarz prinzipiell.",
   verhaeltnis: "7:11"
 
 }
@@ -92,7 +93,7 @@ export const Finland : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Finland.jpg",
   population: "5.584.000 ",
   datum: new Date("1918-06-28"),
-  review: "",
+  review: "Ähnlich wie Estland. Sieht einfach mächtig aus aufgrund des simplen, aber wirkvollen Designs. Blau/Weiß überbietet alles.",
   verhaeltnis: "7:11"
 
 }
@@ -103,7 +104,8 @@ export const Nepal : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Nepal.jpg",
   population: "30.900.000 ",
   datum: new Date("1962-12-16"),
-  review: "",
+  review: "\n" +
+    "Nepal ist die einzige Flagge die nicht 4eckig ist. Das ist so ziemlich die einzige Besonderheit. Farben wirken sehr gut. Mehr ist leider aber auch nicht dabei.",
   verhaeltnis: "1:1.21901033…"
 
 }
@@ -114,7 +116,8 @@ export const Turkmenistan : flaggen = {
   imagelink: "/assets/Kovacs_Bilder/flags/Turkmenistan.jpg",
   population: "6.516.000",
   datum: new Date("1992-10-27"),
-  review: "",
+  review: "\n" +
+    "Meine Lieblingsflagge mit hauptsächlich grünem Untergrund. Der Teppichstreifen passt zu dem, was man mit der Kultur in Verbindung bringt. Daher Top Flagge.",
   verhaeltnis: "2:3"
 
 }
--- src/app/pages/pages-students/pages-kovacs/page-heimat-kovacs/page-heimat-kovacs.component.ts
+++ src/app/pages/pages-students/pages-kovacs/page-heimat-kovacs/page-heimat-kovacs.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {DatePipe} from '@angular/common';
