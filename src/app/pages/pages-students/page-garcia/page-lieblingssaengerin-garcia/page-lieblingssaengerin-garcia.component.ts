3a57e78d561efa7da404a5f7f0654e412a8e38d3
rizi06 <200005@studierende.htl-donaustadt.at>
2025-03-10 09:05:19 +0100

    Scrum 30 Header für alle Seiten

--- src/app/pages/pages-students/page-garcia/page-lieblingssaengerin-garcia/page-lieblingssaengerin-garcia.component.ts
+++ src/app/pages/pages-students/page-garcia/page-lieblingssaengerin-garcia/page-lieblingssaengerin-garcia.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {GARCIA, Person} from '../../../../model/person';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {SZA} from '../sza_info';
+import {PageHeaderGarciaComponent} from "../page-header-garcia/page-header-garcia.component";
 
 @Component({
   selector: 'app-page-lieblingssaengerin-garcia',
-  imports: [
-    RouterLink,
-    RouterLinkActive
-  ],
+    imports: [
+        RouterLink,
+        RouterLinkActive,
+        PageHeaderGarciaComponent
+    ],
   templateUrl: './page-lieblingssaengerin-garcia.component.html',
   standalone: true,
   styleUrl: './page-lieblingssaengerin-garcia.component.scss'
