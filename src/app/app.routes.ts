2c4a34ef4641d07ef89cee777601acd98ade3823
KovacsPhilip <210015@studierende.htl-donaustadt.at>
2025-03-10 23:48:31 +0100

    SCRUM 58

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,9 +13,9 @@ import {PageAboutmeNovoComponent} from "./pages/pages-students/page-novo/page-ab
 import {PageAboutmeMETZComponent} from "./pages/pages-students/pages-metz/page-aboutme-metz/page-aboutme-metz.component";
 import {PageAboutMeSCHOENComponent} from './pages/pages-students/page-about-me-schoengrundner/page-about-me-schoen.component';
 import {PageAboutMeGARCIAComponent} from './pages/pages-students/page-garcia/page-about-me-garcia/page-about-me-garcia.component';
-import {PageAboutMePojarComponent} from "./pages/pages-students/page-about-me-pojar/page-about-me-pojar.component";
-import {PageGetesteteDoenerPojarComponent} from "./pages/pages-students/page-about-me-pojar/page-getestete-doener-pojar/page-getestete-doener-pojar.component";
-import {PageLieblingsessenPojarComponent} from "./pages/pages-students/page-about-me-pojar/page-lieblingsessen-pojar/page-lieblingsessen-pojar.component";
+import {PageAboutMePojarComponent} from './pages/pages-students/pages-pojar/page-about-me-pojar/page-about-me-pojar.component';
+import {PageGetesteteDoenerPojarComponent} from "./pages/pages-students/pages-pojar/page-getestete-doener-pojar/page-getestete-doener-pojar.component";
+import {PageLieblingsessenPojarComponent} from "./pages/pages-students/pages-pojar/page-lieblingsessen-pojar/page-lieblingsessen-pojar.component";
 import {PageAboutMeKOVACICComponent} from './pages/pages-students/pages-kovacic/page-about-me-kovacic/page-about-me-kovacic.component';
 import {PageAboutMeWAGNComponent} from './pages/pages-students/pages-wagner/page-about-me-wagn/page-about-me-wagn.component';
 import {PageAboutMeMajzelComponent} from './pages/pages-students/pages_majzel/page-about-me-majzel/page-about-me-majzel.component';
