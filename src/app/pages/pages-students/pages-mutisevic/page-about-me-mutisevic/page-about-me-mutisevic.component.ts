7a9008f036e9d8a340adb1d8231e8079454b9a8b
adinocarti <200224@studierende.htl-donaustadt.at>
2025-03-10 09:49:37 +0100

    WEB-22, WEB-36, WEB-37 Footer erstellt

--- src/app/pages/pages-students/pages-mutisevic/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-about-me-mutisevic/page-about-me-mutisevic.component.ts
@@ -3,10 +3,11 @@ import {htl} from '../htl';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MUTISEVIC} from '../../../../model/person';
 import {PageHeaderMutisevicComponent} from '../page-header-mutisevic/page-header-mutisevic.component';
+import {PageFooterMutisevicComponent} from '../page-footer-mutisevic/page-footer-mutisevic.component';
 
 @Component({
   selector: 'app-page-about-me-mutisevic',
-  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent],
+  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent, PageFooterMutisevicComponent],
   templateUrl: './page-about-me-mutisevic.component.html',
   standalone: true,
   styleUrl: './page-about-me-mutisevic.component.scss'
--- src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
+++ src/app/pages/pages-students/pages-mutisevic/page-music-mutisevic/page-music-mutisevic.component.ts
@@ -3,10 +3,11 @@ import {carti} from '../carti';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MUTISEVIC} from '../../../../model/person';
 import {PageHeaderMutisevicComponent} from '../page-header-mutisevic/page-header-mutisevic.component';
+import {PageFooterMutisevicComponent} from '../page-footer-mutisevic/page-footer-mutisevic.component';
 
 @Component({
   selector: 'app-page-music-mutisevic',
-  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent],
+  imports: [RouterLink, RouterLinkActive, PageHeaderMutisevicComponent, PageFooterMutisevicComponent],
   templateUrl: './page-music-mutisevic.component.html',
   standalone: true,
   styleUrl: './page-music-mutisevic.component.scss'
