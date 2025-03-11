56fe61d37b232a4285b82e091746fa1ebf94702f
inesschoen <210179@studierende.htl-donaustadt.at>
2025-03-11 12:33:38 +0100

    Geburtsdatum änderung

--- src/app/pages/pages-students/page-about-me-schoengrundner/page-about-me-schoen.component.ts
+++ src/app/pages/pages-students/page-about-me-schoengrundner/page-about-me-schoen.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {SCHOENGRUNDNER, Person} from '../../../model/person';
+import {DatePipe, formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-page-about-me-schoen',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    DatePipe
   ],
   templateUrl: './page-about-me-schoen.component.html',
   standalone: true,
@@ -15,4 +17,5 @@ import {SCHOENGRUNDNER, Person} from '../../../model/person';
 export class PageAboutMeSCHOENComponent {
   me_schoen: Person = SCHOENGRUNDNER;
   protected readonly me = SCHOENGRUNDNER;
+  protected readonly formatDate = formatDate;
 }
